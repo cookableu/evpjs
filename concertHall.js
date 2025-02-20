83a37f6105f4d042fa22ef158dfcde3124403360
cookableu <138080532+cookableu@users.noreply.github.com>
2025-02-20 15:21:17 +0000

    Add files via upload

---
+++ concertHall.js
@@ -0,0 +1,31 @@
+function revealSearch() {
+      document.getElementsByClassName("searchIcon")[0].style.display = "block";
+    }
+
+    function hideSearch() {
+      document.getElementsByClassName("searchIcon")[0].style.display = "none";
+    }
+
+    function revealSearch2() {
+      document.getElementsByClassName("searchIcon")[1].style.display = "block";
+    }
+
+    function hideSearch2() {
+      document.getElementsByClassName("searchIcon")[1].style.display = "none";
+    }
+
+    function revealSearch3() {
+      document.getElementsByClassName("searchIcon")[2].style.display = "block";
+    }
+
+    function hideSearch3() {
+      document.getElementsByClassName("searchIcon")[2].style.display = "none";
+    }
+
+    function revealSearch4() {
+      document.getElementsByClassName("searchIcon")[3].style.display = "block";
+    }
+
+    function hideSearch4() {
+      document.getElementsByClassName("searchIcon")[3].style.display = "none";
+    }
